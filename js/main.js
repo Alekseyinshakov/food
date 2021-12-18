6d5c8a0eb2b5ee5178dacd06beb06827a57ac649
Alexey <alexeyinshakov992@gmail.com>
2021-12-18 19:42:54 +0300

    kards add

--- js/main.js
+++ js/main.js
@@ -30,25 +30,24 @@ const deadLine = '2021-12-27';
 
 function getTimeRemaining(endtime) {
   const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor(t/(1000*60*60*24)),
-        hours = Math.floor((t%(1000*60*60*24))/(1000*60*60)),
-        minutes = Math.floor((t%(1000*60*60))/(1000*60)),
-        seconds = Math.floor((t%(1000*60))/1000)
-        
-        return {
-          total: t,
-          days: days,
-          hours: hours,
-          minutes: minutes,
-          seconds: seconds
-        }
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+    hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds = Math.floor((t % (1000 * 60)) / 1000)
+
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  }
 }
 
 function getZiro(num) {
   if (num < 10) {
     return '0' + num
-  }
-  else {
+  } else {
     return num
   }
 }
@@ -56,25 +55,26 @@ function getZiro(num) {
 
 function setClock(selector, endtime) {
   const timer = document.querySelector(selector),
-        days = timer.querySelector('#days'),
-        hours = timer.querySelector('#hours'),
-        minutes = timer.querySelector('#minutes'),
-        seconds = timer.querySelector('#seconds')
-        timeInterval = setInterval(updateClock, 1000);
+    days = timer.querySelector('#days'),
+    hours = timer.querySelector('#hours'),
+    minutes = timer.querySelector('#minutes'),
+    seconds = timer.querySelector('#seconds')
+  timeInterval = setInterval(updateClock, 1000);
 
-        updateClock()
-        function updateClock() {
-          const t = getTimeRemaining(endtime)
+  updateClock()
 
-          days.innerHTML = getZiro(t.days)
-          hours.innerHTML = getZiro(t.hours)
-          minutes.innerHTML = getZiro(t.minutes)
-          seconds.innerHTML = getZiro(t.seconds)
+  function updateClock() {
+    const t = getTimeRemaining(endtime)
 
-          if (t.total <= 0) {
-            clearInterval(timeInterval)
-          }
-        }
+    days.innerHTML = getZiro(t.days)
+    hours.innerHTML = getZiro(t.hours)
+    minutes.innerHTML = getZiro(t.minutes)
+    seconds.innerHTML = getZiro(t.seconds)
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval)
+    }
+  }
 }
 
 setClock('.timer', deadLine)
@@ -84,7 +84,7 @@ setClock('.timer', deadLine)
 const modal = document.querySelector('.modal')
 const openModal = document.querySelectorAll('[data-modal]')
 const closeModal = document.querySelector('[data-close]')
-const modalTimerId = setTimeout(openModalFunction, 5000)
+const modalTimerId = setTimeout(openModalFunction, 500000)
 
 openModal.forEach(item => {
   item.addEventListener('click', () => {
@@ -102,7 +102,7 @@ modal.addEventListener('click', (e) => {
   }
 })
 
-document.addEventListener('keydown', (e) => {  
+document.addEventListener('keydown', (e) => {
   if (e.code === 'Escape' && modal.style.display) {
     closeModalFunction();
   }
@@ -112,11 +112,11 @@ window.addEventListener('scroll', showModalByScroll)
 
 function openModalFunction(params) {
   modal.style.display = 'block'
-    document.body.style.overflow = 'hidden'
+  document.body.style.overflow = 'hidden'
 }
 
 function showModalByScroll(params) {
-  if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+  if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
     openModalFunction();
     removeEventListener('scroll', showModalByScroll)
   }
@@ -127,3 +127,48 @@ function closeModalFunction() {
   document.body.style.overflow = ''
   clearTimeout(modalTimerId)
 }
+
+//*********************** CARDS************************ */
+const arrayProps = [
+  ['img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229],
+  ['img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550],
+  ['img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 430]
+];
+const cardsContainer = document.querySelector('.menu .container');
+cardsContainer.innerHTML = '';
+
+class Cards {
+  constructor(img, alt, title, descr, price, parent) {
+    this.img = img;
+    this.alt = alt;
+    this.title = title;
+    this.descr = descr;
+    this.price = price;
+    this.transfer = 60;
+    this.parent = parent;
+    this.changeToRub();
+  }
+  changeToRub() {
+    this.price = this.price * this.transfer;
+  }
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = `<div class="menu__item">
+    <img src="${this.img}" alt="${this.alt}">
+    <h3 class="menu__item-subtitle">${this.title}</h3>
+    <div class="menu__item-descr">${this.descr}</div>
+    <div class="menu__item-divider"></div>
+    <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+    </div>
+    </div>`;
+    this.parent.append(element);
+  }
+} 
+
+new Cards('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, cardsContainer).render()
+
+new Cards('img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550, cardsContainer).render()
+
+new Cards('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ', 430, cardsContainer).render()
